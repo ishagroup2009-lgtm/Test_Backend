98cf7ec4c60bf0cb19b4727312757ef6ceb8731f
ISHA Technology <ishagroup2009@gmail.com>
2026-03-09 13:54:00 +0530

    Message schema chnage

--- models/Message.js
+++ models/Message.js
@@ -10,7 +10,8 @@ const MessageSchema = new mongoose.Schema(
     receiverId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
-      required: true,
+      // required: true,
+      default: null
     },
     groupId: {
       type: mongoose.Schema.Types.ObjectId,
